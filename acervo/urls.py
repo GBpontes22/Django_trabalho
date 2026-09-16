da1469bb921207e25cd45e5f4e3560613445f729
GBpontes22 <GBpontes22@users.noreply.github.com>
2026-09-16 20:41:31 -0300

    Add acervo list and create views

--- acervo/urls.py
+++ acervo/urls.py
@@ -6,5 +6,6 @@ app_name = 'acervo'
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('livros/', views.lista_livros, name='lista'),
+    path('livros/novo/', views.novo_livro, name='novo'),
 ]
-
--- acervo/views.py
+++ acervo/views.py
@@ -1,7 +1,47 @@
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from .forms import LivroForm
+from .models import Livro
 
 
 def inicio(request):
-    return HttpResponse('Ola, acervo!')
+    return redirect('acervo:lista')
+
+
+def lista_livros(request):
+    busca = request.GET.get('q', '').strip()
+    tipo = request.GET.get('tipo', '').strip()
+    categoria = request.GET.get('categoria', '').strip()
+
+    livros = Livro.objects.all()
+
+    if busca:
+        livros = livros.filter(titulo__icontains=busca)
+    if tipo:
+        livros = livros.filter(tipo_acervo=tipo)
+    if categoria:
+        livros = livros.filter(categoria=categoria)
+
+    contexto = {
+        'livros': livros,
+        'busca': busca,
+        'tipo_selecionado': tipo,
+        'categoria_selecionada': categoria,
+        'tipos_acervo': Livro.TIPO_ACERVO_CHOICES,
+        'categorias': Livro.CATEGORIA_CHOICES,
+    }
+    return render(request, 'acervo/lista.html', contexto)
+
+
+def novo_livro(request):
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('acervo:lista')
+    else:
+        form = LivroForm()
+
+    return render(request, 'acervo/form.html', {'form': form})
 
 # Create your views here.

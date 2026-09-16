fcbc3c2567acb6e3dbe7e2415ec81b75b25e508c
GBpontes22 <GBpontes22@users.noreply.github.com>
2026-09-16 20:40:48 -0300

    Register Livro in Django admin

--- acervo/admin.py
+++ acervo/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import Livro
+
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = (
+        'titulo',
+        'autor',
+        'ano',
+        'tipo_acervo',
+        'categoria',
+        'disponivel',
+    )
+    list_filter = ('tipo_acervo', 'categoria', 'disponivel')
+    search_fields = ('titulo', 'autor')
+    ordering = ('titulo',)
+
 # Register your models here.
